0f473dd974b1f9b3b308ccf2a0358ffb879b1739
Evgeny Storozhenko <evgeniy.storozhenko@gmail.com>
2015-03-16 15:23:05 +0600

    Change maven configuration

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export MAVEN_OPTS="-Xms512m -Xmx756m -XX:MaxPermSize=256m"
-
+# export MAVEN_OPTS="-Xms512m -Xmx756m -XX:MaxPermSize=256m"
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_31.jdk/Contents/Home
 mvn clean verify
 
